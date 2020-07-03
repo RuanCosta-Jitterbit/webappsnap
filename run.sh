04b415d48e177eb9be98eac2e3dc826b916c752c
Paul Jacobs <paul.jacobs@percona.com>
2020-07-03 17:06:07 +0300

    Update run.sh
    Move parameters to config file.
    Save as date dirs

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #! env bash
-# Wrapper script for auto screen capture using Puppeteer
+# Wrapper script for auto screen capture using Puppeteer TODO use python?
 set -a
 
 # PMM Server credentials (default admin/admin)
 # USER=admin
-# Specify externally for security
-#PASS=
+# PASS=
 
+# NOTES
 # 4:3   : 640×480, 800×600, 960×720, 1024×768,           1280×960, 1400×1050, 1440×1080 , 1600×1200, 1856×1392, 1920×1440, 2048×1536
 # 16:10 :                                                1280×800,            1440×900,        1680×1050,       1920×1200,       2560×1600
 # 16:9  :                            1024×576, 1152×648, 1280×720, 1366×768,              1600×900,             1920×1080,       2560×1440, 3840×2160
@@ -21,14 +21,10 @@ set -a
 # Image scale factor (default: 1)
 #SCALE=0.5
 
-# Root for images (saved in IMGDIR/WIDTHxHEIGHT/SCALE/)
-IMGDIR="images" 
+# Root for images (default 'images') saved in IMGDIR/WIDTHxHEIGHT/SCALE/
+IMGDIR=$(date +%Y%m%d)
 
-
-# TODO specify config file
-
-time node screen-capture-pmm.js
-
+time node screen-capture-pmm.js $1
 
 # TODO use imagemagick to create gif slide show
 # Delay in 1/100s
