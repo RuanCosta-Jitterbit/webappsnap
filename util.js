2b196af0e75053b864ad4016c19235ef169fd4a7
Paul Jacobs <paul.jacobs@percona.com>
2020-07-06 21:11:01 +0300

    Update util.js
    Headless mode

--- util.js
+++ util.js
@@ -12,9 +12,10 @@ const config = require(config_file);
 
 // Override defaults.json values with env vars, if set
 const img_dir     = process.env.SNAP_IMGDIR || defaults.img_dir;
-const jpg_quality = Number(process.env.SNAP_JPG_QUALITY) || defaults.jpg_quality; // Only jpg uses quality factor
+const jpg_quality = Number(process.env.SNAP_JPG_QUALITY) || defaults.jpg_quality; // jpg only
 const user        = process.env.SNAP_USER || defaults.user;
 const pass        = process.env.SNAP_PASS || defaults.pass;
+const headless    = ((process.env.SNAP_HEADLESS) ? (process.env.SNAP_HEADLESS == 'true') : (defaults.headless === true));
 const img_pfx     = process.env.SNAP_IMG_PFX || defaults.img_pfx;
 const img_ext     = process.env.SNAP_IMG_EXT || defaults.img_ext;
 const img_width   = Number(process.env.SNAP_IMG_WIDTH) || defaults.img_width;
@@ -22,7 +23,6 @@ const img_height  = Number(process.env.SNAP_IMG_HEIGHT) || defaults.img_height;
 const img_scale   = Number(process.env.SNAP_IMG_SCALE) || defaults.img_scale; 
 
 // Get values from server config
-//const url = config.server;
 const hostname = uf.parse(config.server).hostname;
 
 // Image save directory
@@ -102,6 +102,7 @@ module.exports.config_file = config_file;
 module.exports.hostname = hostname;
 module.exports.user = user;
 module.exports.pass = pass;
+module.exports.headless = headless;
 module.exports.img_height = img_height;
 module.exports.img_width = img_width;
 module.exports.img_scale = img_scale;
