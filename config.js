5c474b2a4cd2f414d8b82e4f59d97cccf3165742
Paul Jacobs <paul.jacobs@percona.com>
2020-08-25 15:13:02 +0300

    Special treatment for prefix; log in flag change

--- config.js
+++ config.js
@@ -15,16 +15,34 @@ const server_cfg = require(cfg_file_name);
 const hostname = uf.parse(server_cfg.server).hostname; // The PMM URL/server/IP
 
 // Override defaults.json values with env vars, if set
-const jpg_quality = Number(process.env.SNAP_JPG_QUALITY) || defaults.jpg_quality; // jpg only
-const user        = process.env.SNAP_USER || defaults.user;
-const pass        = process.env.SNAP_PASS || defaults.pass;
-const headless    = ((process.env.SNAP_HEADLESS) ? (process.env.SNAP_HEADLESS == 'true') : (defaults.headless === true));
-const img_pfx     = process.env.SNAP_IMG_PFX || defaults.img_pfx;
-const img_seq     = ((process.env.SNAP_IMG_SEQ) ? (process.env.SNAP_IMG_SEQ == 'true') : (defaults.img_seq === true));
-const img_ext     = process.env.SNAP_IMG_EXT || defaults.img_ext;
-const img_width   = Number(process.env.SNAP_IMG_WIDTH) || defaults.img_width;
-const img_height  = Number(process.env.SNAP_IMG_HEIGHT) || defaults.img_height;
-const img_scale   = Number(process.env.SNAP_IMG_SCALE) || defaults.img_scale;
+var jpg_quality = Number(process.env.SNAP_JPG_QUALITY) || defaults.jpg_quality; // jpg only
+
+// Prefix is special because it can be empty. Node.js can't distinguish between unset and empty.
+var img_pfx = null;
+if (typeof process.env.SNAP_IMG_PFX === 'undefined') {
+    img_pfx = defaults.img_pfx;
+} else if (typeof process.env.SNAP_IMG_PFX === 'string' && process.env.SNAP_IMG_PFX.length === 0) {
+    img_pfx = '';
+} else if (typeof process.env.SNAP_IMG_PFX === 'string' && process.env.SNAP_IMG_PFX.length > 0) {
+    img_pfx = process.env.SNAP_IMG_PFX;
+} else {
+    console.log("SNAP_IMG_PFX can't be set");
+}
+
+
+
+var img_ext     = process.env.SNAP_IMG_EXT || defaults.img_ext;
+var img_width   = Number(process.env.SNAP_IMG_WIDTH) || defaults.img_width;
+var img_height  = Number(process.env.SNAP_IMG_HEIGHT) || defaults.img_height;
+var img_scale   = Number(process.env.SNAP_IMG_SCALE) || defaults.img_scale;
+
+var user        = process.env.SNAP_USER || defaults.user;
+var pass        = process.env.SNAP_PASS || defaults.pass;
+// Binary options
+var img_seq     = ((process.env.SNAP_IMG_SEQ) ? (process.env.SNAP_IMG_SEQ == 'true') : (defaults.img_seq === true));
+var headless    = ((process.env.SNAP_HEADLESS) ? (process.env.SNAP_HEADLESS == 'true') : (defaults.headless === true));
+var log_in      = ((process.env.SNAP_LOG_IN) ? (process.env.SNAP_LOG_IN == 'true') : (defaults.log_in === true));
+var debug       = ((process.env.SNAP_DEBUG) ? (process.env.SNAP_DEBUG == 'true') : (defaults.debug === true));
 
 // Base dir for images
 const img_dir = process.env.SNAP_IMG_DIR || defaults.img_dir;
@@ -40,6 +58,8 @@ module.exports.defaults_file_name = defaults_file_name;
 module.exports.hostname = hostname;
 module.exports.user = user;
 module.exports.pass = pass;
+module.exports.log_in = log_in;
+module.exports.debug = debug;
 module.exports.headless = headless;
 module.exports.img_height = img_height;
 module.exports.img_width = img_width;
